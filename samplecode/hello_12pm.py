615e51ecbc698c366a50c78d3cf10e16864530e9
David Dumas <david@dumas.io>
2023-01-11 14:15:38 -0600

    Sample code from lecture 2: live tour notebooks

---
+++ samplecode/hello_12pm.py
@@ -0,0 +1,7 @@
+"Example Python script (to contrast to REPL)"
+# The previous line is a docstring or documentation string
+
+# now the actual work
+2**1000
+3 + 8
+print("Hello world")
---
+++ samplecode/hello_1pm.py
@@ -0,0 +1,7 @@
+# MCS 275 Spring 2023 Lecture 2 Example
+# David Dumas
+"Sample script to contrast with interactive mode"
+
+print("Hello world")
+17 + 29
+print(2**500)
