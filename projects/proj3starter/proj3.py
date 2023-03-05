2885b818953066d623319a3ea6d34c2fe7f6d5ee
David Dumas <david@dumas.io>
2023-03-05 11:07:51 -0600

    Project 3 starter pack

---
+++ projects/proj3starter/proj3.py
@@ -0,0 +1,29 @@
+# HEADER HERE (and delete this line)
+# This single file is the MCS 275 Spring 2023 Project 3 Starter Pack
+# Add stuff to it and submit to Gradescope
+
+class KVNode:
+    "Node in a binary tree with both key and value"
+    # Add implementation here
+
+
+class KVBST(KVNode):
+    "Key-value binary search tree (with recursive insert, search)"
+    # Add implementation here
+
+
+class Dictionary:
+    "Partial `dict` clone using `KVBST` as backing data store"
+    # Add implementation here
+
+
+if __name__ == "__main__":
+    # PUT TEST CODE HERE
+    # Must all be in this indented block
+    # which runs in script mode, not when
+    # imported as a module.
+    pass
+
+
+# Make sure there are no unindented test statements! They
+# would run when this is imported as a module.
